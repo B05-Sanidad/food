64de377ff103f229160301730eda8186e57402cc
B05 <b05@b05.es>
2025-10-12 21:42:33 +0200

    new stuff

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 // config.js
 window.B05_CONFIGS = {
-  HOMMUS: {
+    "5HUM517": {
     Food: "HOMMUS",
     PREFIX: "5-HOM-",
     latestLotNumber: "5-HOM-517",
-    PRODUCTO: "Garbanzos cocidos, Tahini, Limón, Aceite de oliva, Ajo, Sal",
+    PRODUCTO: "aire Garbanzos cocidos, Tahini, Limón, Aceite de oliva, Ajo, Sal",
     ORIGEN: "Makro / Oasis / Consum",
     LOTE: "Garbanzos L778, Tahini L120, Aceite L221",
     USADO: "Hummus",
@@ -14,31 +14,31 @@ window.B05_CONFIGS = {
     USE_DAYS: [2, 5]
   },
 
-  BERENJENA: {
+  "11BER610": {
     Food: "BERENJENA",
     PREFIX: "5-BER-",
-    latestLotNumber: "5-BER-321",
+    latestLotNumber: "11-BER-610",
     PRODUCTO: "Berenjena asada, Ajo, Limón, Tahini, Aceite de oliva, Sal",
     ORIGEN: "Makro / Consum / Oasis",
     LOTE: "Berenjena L902, Tahini L130",
     USADO: "Baba Ghanoush",
     CHEF: "Chef B05",
-    EXP_DAYS: 3,
-    MONTHS_BACK: 6,
-    USE_DAYS: [2, 5]
-  },
-
-  LABNEH: {
-    Food: "LABNEH",
-    PREFIX: "5-LAB-",
-    latestLotNumber: "5-LAB-222",
-    PRODUCTO: "Yogur colado, Sal, Aceite de oliva",
-    ORIGEN: "Makro / Carrefour",
-    LOTE: "Yogur L400",
-    USADO: "Labneh Bowl",
-    CHEF: "Chef B05",
     EXP_DAYS: 5,
     MONTHS_BACK: 6,
-    USE_DAYS: [1, 4] // Lunes y Jueves
+    USE_DAYS: [4]
+  },
+
+  "29MUH430": {
+    Food: "MOUHAMARA",
+    PREFIX: "5-LAB-",
+    latestLotNumber: "5-LAB-222",
+    PRODUCTO: "mouhamaara colado, Sal, Aceite de oliva",
+    ORIGEN: "Makro / Carrefour",
+    LOTE: "Yogur L400",
+    USADO: "moujhan Bowl",
+    CHEF: "Chef B05",
+    EXP_DAYS: 10,
+    MONTHS_BACK: 6,
+    USE_DAYS: [1] // Lunes 
   }
 };
