edf85c58672d7fc6c1876dad7e758160092a7c05
B05 <b05@b05.es>
2025-10-12 20:27:35 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,44 @@
+// config.js
+window.B05_CONFIGS = {
+  HOMMUS: {
+    Food: "HOMMUS",
+    PREFIX: "5-HOM-",
+    latestLotNumber: "5-HOM-517",
+    PRODUCTO: "Garbanzos cocidos, Tahini, Limón, Aceite de oliva, Ajo, Sal",
+    ORIGEN: "Makro / Oasis / Consum",
+    LOTE: "Garbanzos L778, Tahini L120, Aceite L221",
+    USADO: "Hummus",
+    CHEF: "Chef B05",
+    EXP_DAYS: 4,
+    MONTHS_BACK: 6,
+    USE_DAYS: [2, 5]
+  },
+
+  BERENJENA: {
+    Food: "BERENJENA",
+    PREFIX: "5-BER-",
+    latestLotNumber: "5-BER-321",
+    PRODUCTO: "Berenjena asada, Ajo, Limón, Tahini, Aceite de oliva, Sal",
+    ORIGEN: "Makro / Consum / Oasis",
+    LOTE: "Berenjena L902, Tahini L130",
+    USADO: "Baba Ghanoush",
+    CHEF: "Chef B05",
+    EXP_DAYS: 3,
+    MONTHS_BACK: 6,
+    USE_DAYS: [2, 5]
+  },
+
+  LABNEH: {
+    Food: "LABNEH",
+    PREFIX: "5-LAB-",
+    latestLotNumber: "5-LAB-222",
+    PRODUCTO: "Yogur colado, Sal, Aceite de oliva",
+    ORIGEN: "Makro / Carrefour",
+    LOTE: "Yogur L400",
+    USADO: "Labneh Bowl",
+    CHEF: "Chef B05",
+    EXP_DAYS: 5,
+    MONTHS_BACK: 6,
+    USE_DAYS: [1, 4] // Lunes y Jueves
+  }
+};
