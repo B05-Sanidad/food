f6fac0f10ac1a354a96ee628cc3a8653e7360b08
B05 <b05@b05.es>
2025-10-14 22:43:52 +0200

    Update config.js

--- config.js
+++ config.js
@@ -57,21 +57,6 @@ window.B05_CONFIGS = {
     PROD_ANCHOR: "2025-08-15"
   },
 
-  "29MUH431": {
-    Food: "LOVEME",
-    PREFIX: "29-MUH-",
-    latestLotNumber: "29-MUH-431",
-    PRODUCTO: "Love roja, Nueces, Melaza de granada, Pan rallado, Aceite, Especias, Sal",
-    ORIGEN: "Makro / Carrefour / Oasis",
-    LOTE: "Pimiento L431, Nueces L221",
-    USADO: "AIRE",
-    CHEF: "Chef B05",
-    EXP_DAYS: 12,
-    MONTHS_BACK: 6,
-    DAYS_PRODUCTION: 20,
-    PROD_ANCHOR: "2025-07-25"
-  },
-
   "11PAR654": {
     Food: "HOJAS DE PARRA",
     PREFIX: "11-PAR-",
@@ -93,7 +78,7 @@ window.B05_CONFIGS = {
     latestLotNumber: "17-SHC-544",
     PRODUCTO: "Carne de ternera, cebolla, tomate, yogur, vinagre,limón, especias, ajo, sal",
     ORIGEN: "Makro / Oasis / Consum",
-    LOTE: "Carne L7021, Esp Oasis L440",
+    LOTE: "Carne L7021, Esp Oasis Oasis",
     USADO: "Sh. Carne",
     CHEF: "Chef B05",
     EXP_DAYS: 12,
@@ -107,7 +92,7 @@ window.B05_CONFIGS = {
     latestLotNumber: "23-BQP-533",
     PRODUCTO: "Pechuga de pollo, yogur, vinagre, limón, aceite de oliva, ajo, especias, sal",
     ORIGEN: "Makro / Oasis / Consum",
-    LOTE: "Pollo L7022, Especias L440",
+    LOTE: "Pollo L7022, Especias Oasis",
     USADO: "BBQ Pollo",
     CHEF: "Chef B05",
     EXP_DAYS: 14,
@@ -122,7 +107,7 @@ window.B05_CONFIGS = {
     latestLotNumber: "25-BQC-342",
     PRODUCTO: "Carne , yogur, vinagre, limón, aceite de oliva, ajo, especias, cebolla, sal",
     ORIGEN: "Makro / Oasis / Consum",
-    LOTE: "CARNE ESP MAKRO, Especias L440",
+    LOTE: "CARNE ESP MAKRO, Especias Oasis",
     USADO: "BBQ CARNE",
     CHEF: "Chef B05",
     EXP_DAYS: 35,
@@ -136,7 +121,7 @@ window.B05_CONFIGS = {
     latestLotNumber: "9-KEB-345",
     PRODUCTO: "Carne de ternera magra, bulgur (trigo partido fino), cebolla, piñones",
     ORIGEN: "Makro / Oasis / Consum",
-    LOTE: "CARNE - KEBBE , Especias L440",
+    LOTE: "CARNE - KEBBE , Especias Oasis",
     USADO: "KEBBE",
     CHEF: "Chef B05",
     EXP_DAYS: 40,
@@ -161,12 +146,12 @@ window.B05_CONFIGS = {
   }, 
 
   "8RAK934": {
-    Food: "RAKA",
+    Food: "RAKA QUESO",
     PREFIX: "8RAK-",
     latestLotNumber: "8-RAK-934",
     PRODUCTO: "Queso blanco, mozzarella, harina de trigo, agua, sal, aceite de girasol",
     ORIGEN: "Makro / Oasis / Consum",
-    LOTE: "Queso Makro , Especias L440",
+    LOTE: "Queso Makro , Especias Oasis",
     USADO: "RAKA",
     CHEF: "Chef B05",
     EXP_DAYS: 45,
@@ -174,6 +159,8 @@ window.B05_CONFIGS = {
     DAYS_PRODUCTION: 40,
     PROD_ANCHOR: "2025-10-10"
   }, 
+
+
    
    "51SAQ423": {
     Food: "SAMB-QUESO",
@@ -181,7 +168,21 @@ window.B05_CONFIGS = {
     latestLotNumber: "51-SAQ-423",
     PRODUCTO: "Harina de trigo, Queso blanco, mozzarella, harina de trigo, agua, sal",
     ORIGEN: "Makro / Oasis / Consum",
-    LOTE: "Queso Makro , Especias L440",
+    LOTE: "Queso Makro , Especias Oasis",
+    USADO: "SAMB-QUESO",
+    CHEF: "Chef B05",
+    EXP_DAYS: 60,
+    MONTHS_BACK: 6,
+    DAYS_PRODUCTION: 40,
+    PROD_ANCHOR: "2025-10-10"
+  }, 
+   "22BQK543": {
+    Food: "KAFTA",
+    PREFIX: "22-BQK-",
+    latestLotNumber: "22-BQK-543",
+    PRODUCTO: "carne de vacuno, cebolla, perejil, sal, especias"
+    ORIGEN: "Makro / Oasis / Consum",
+    LOTE: "carne Makro , Especias Oasis",
     USADO: "SAMB-QUESO",
     CHEF: "Chef B05",
     EXP_DAYS: 60,
@@ -196,7 +197,7 @@ window.B05_CONFIGS = {
     latestLotNumber: "51-SAC-234",
     PRODUCTO: "Harina de trigo, Carne Ternera, harina de trigo, agua, sal, aceite de girasol",
     ORIGEN: "Makro / Oasis / Consum",
-    LOTE: "Carne Makro , Especias L440",
+    LOTE: "Carne Makro , Especias OASIS",
     USADO: "SAM-CARNE",
     CHEF: "Chef B05",
     EXP_DAYS: 60,
@@ -211,7 +212,7 @@ window.B05_CONFIGS = {
     latestLotNumber: "53-FAT-243",
     PRODUCTO: "Harina de trigo, espinacas frescas, cebolla, limón, sal, pimienta",
     ORIGEN: "Makro / Oasis / Consum",
-    LOTE: "espinacas , Especias L440",
+    LOTE: "espinacas , Especias Oasis",
     USADO: "FATAYER",
     CHEF: "Chef B05",
     EXP_DAYS: 60,
